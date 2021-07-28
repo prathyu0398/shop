47c6e65ad8aa1da098fb752639b86ed2a7bf5810
prathyu0398 <prathyu0398@gmail.com>
2021-07-28 21:25:09 +0530

    first push

---
+++ src/main/java/com/cognizant/test/TestApplication.java
@@ -0,0 +1,28 @@
+package com.cognizant.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@SpringBootApplication
+@EnableAutoConfiguration
+@RestController
+@RequestMapping("/test")
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+	
+	@GetMapping
+	public String applicationStatus() {
+		return "</h2>Application is up and running</h2>";
+	}
+	
+	
+
+}
